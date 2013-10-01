1c8753658eb4046f999e76058c14b39867e7c86d
Conent <albertocontento@gmail.com>
2013-10-01 16:16:56 +0100

    itital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendmap::Application.config.secret_token = '67f17f91dc4f3d92d1159c052baacfe886753927a1740cf3ecb86158f88c46265996049b944ce85f2a4d51b4d8b3cb950265bfa4544616adf522ecb39660028f'
