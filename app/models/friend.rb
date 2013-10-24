935f9e43be126859c55dd0bf7cc78c53190e5822
Conent <albertocontento@gmail.com>
2013-10-24 16:34:33 +0100

    tolto il magheggio per avere il current frient in mondel

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,4 @@
 class Friend < ActiveRecord::Base
-  attr_accessor :fid
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -15,33 +14,20 @@ class Friend < ActiveRecord::Base
   # attr_accessible :title, :body
 
   has_attached_file :picture, styles: { medium: "256x256>", small: "128x128>", navbar: "35x35>"},
-                              url:"/listimages/:style/:basename.:extension",
-                              path:":rails_root/public/listimages/:style/:basename.:extension"
+                              url:"/listimages/:style/:normalized_picture_file_name.:extension",
+                              path:":rails_root/public/listimages/:style/:normalized_picture_file_name.:extension"
   validates_attachment :picture, content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                                  size: { less_than: 5.megabytes }
   has_many :friendships, :dependent => :destroy
   has_many :friend_requests, :dependent => :destroy
 
-  before_save :change_file_name
 
 
-  def self.current_friend
-    Thread.current[:current_friend]
+  Paperclip.interpolates :normalized_picture_file_name do |attachment, style|
+    attachment.instance.normalized_picture_file_name
   end
 
-  def self.current_friend=(fid)
-    Thread.current[:current_friend] = fid
-  end
- 
-
-private
-
-  def change_file_name
-    if (Friend.current_friend!=nil && picture_file_name!=nil)
-      extension = File.extname(picture_file_name).downcase
-      self.fid= Friend.current_friend.id.to_s
-      self.picture_file_name = fid.concat(".png")  
-    end
-    
+  def normalized_picture_file_name
+    "#{self.id}"
   end
 end
