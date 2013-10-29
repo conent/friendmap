a1a0bd40fe7342087095a27a18be4fd1db2749d7
Conent <albertocontento@gmail.com>
2013-10-29 11:32:03 +0000

    solved 1 problem signin mobile

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -534,7 +534,7 @@ class MobileController < ApplicationController
 				if (f.lastseen == nil)
 					f.update_attributes(:isonline => false)
 				else
-					if ((Time.now - f.lastseen)/1.MINUTE>8)
+					if ((Time.now - f.lastseen)/1.minute > 8)
 						f.update_attributes(:isonline => false)
 					else
 						f.update_attributes(:isonline => false)
