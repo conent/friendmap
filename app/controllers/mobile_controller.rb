b3d7b5e963e07ce66e127039bbfd95bf0cffb7a7
Conent <albertocontento@gmail.com>
2013-11-04 13:52:11 +0000

    tentativio mobile uplodeimage

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -309,8 +309,8 @@ class MobileController < ApplicationController
 					format.json { render json: json}
 				end
 			else
-				f1=Friend.find(id)
-				f1.update_attributes(:picture => params[:picture])
+				bucket = s3.buckets['friendmap']
+				S3Object.store('me.jpg', open(params[:picture]), 'friendmap')
 				
 				# uploaded_io = params[:picture].tmpfile
 				# File.open("https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/small/".concat(params[:picture].original_filename), 'w') do |file|
