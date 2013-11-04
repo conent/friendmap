564248b8539309836104288246c7bfd88f40d81a
Conent <albertocontento@gmail.com>
2013-11-04 12:22:01 +0000

    tentativio mobile uplodeimage

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -310,7 +310,7 @@ class MobileController < ApplicationController
 				end
 			else
 				f1=Friend.find(id)
-				f1.update_attributes(:picture => params[:picture])
+				f1.picture = params[:picture]
 				
 				# uploaded_io = params[:picture].tmpfile
 				# File.open("https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/small/".concat(params[:picture].original_filename), 'w') do |file|
