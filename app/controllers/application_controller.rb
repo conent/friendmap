1fcff450c151a05d29b45154e9becd5e86b809b4
Conent <albertocontento@gmail.com>
2013-11-06 16:56:00 +0000

    fix map sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #not working
-  @friends = Friend.all
-  @hash = Gmaps4rails.build_markers(@friends) do |friend, marker|
-	  marker.lat friend.latitude
-	  marker.lng friend.longitude
-	end
-         
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
   def home
-  	 @pending_requests = FriendRequest.where("otherfriend= ?", current_friend)
+  	@pending_requests = FriendRequest.where("otherfriend= ?", current_friend)
+  	string = ""
+  	@hash = Gmaps4rails.build_markers(Friend.all) do |friend, marker|
+	    if ((friend.latitude != nil && friend.longitude != nil) && !(friend.latitude == 0 && friend.longitude == 0))
+	      marker.lat friend.latitude
+	      marker.lng friend.longitude  
+	      string = friend.name.concat(" ").concat(friend.surname)
+	      marker.infowindow string
+    	end
+	  end
   end
 
   def about
