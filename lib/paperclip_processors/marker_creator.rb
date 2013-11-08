22680a49e4c7092799432fe80a1013703c50b38b
Conent <albertocontento@gmail.com>
2013-11-08 12:01:46 +0000

    random tentaivo di aggiungere i marker su S3

--- lib/paperclip_processors/marker_creator.rb
+++ lib/paperclip_processors/marker_creator.rb
@@ -8,31 +8,20 @@ module Paperclip
       @whiny          = options[:whiny].nil? ? true : options[:whiny]
     end
 
-    def make
-
-
+    def make  
+      @file.rewind # move pointer back to start of file in case handled by other processors
       first_image = MiniMagick::Image.open "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/original/mapmarker.png"
       second_image = MiniMagick::Image.open @file.path
       result = first_image.composite(second_image) do |c|
         c.compose "Over" # OverCompositeOp
         c.geometry "+20+20" # copy second_image onto first_image from (20, 20)
       end
-      result.write "output.jpg"
+      result.write @file.path
       
-        
-          @file.rewind # move pointer back to start of file in case handled by other processors
-          first_image = MiniMagick::Image.open "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/original/mapmarker.png"
-          second_image = MiniMagick::Image.open @file.path
-          result = first_image.composite(second_image) do |c|
-            c.compose "Over" # OverCompositeOp
-            c.geometry "+20+20" # copy second_image onto first_image from (20, 20)
-          end
-          result.write @file.path
-        
-          # tmp = Tempfile.new([@basename, @current_format].compact.join("."))
-          # tmp << file_content
-          # tmp.flush 
-         # @file = tmp
+      # tmp = Tempfile.new([@basename, @current_format].compact.join("."))
+      # tmp << file_content
+      # tmp.flush 
+      # @file = tmp
               
                  
         @file
