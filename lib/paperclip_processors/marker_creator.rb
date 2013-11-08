029349bfba1f0656d05780f0cc7f8f91f947ba73
Conent <albertocontento@gmail.com>
2013-11-08 12:28:11 +0000

    random tentaivo di aggiungere i marker su S3

--- lib/paperclip_processors/marker_creator.rb
+++ lib/paperclip_processors/marker_creator.rb
@@ -9,11 +9,14 @@ module Paperclip
     end
 
     def make  
+      tmp1 = Tempfile.new(["first_image_r", ".png"].compact.join("."))
+      tmp2 = Tempfile.new(["second_image_r", ".png"].compact.join("."))
       @file.rewind # move pointer back to start of file in case handled by other processors
       first_image = MiniMagick::Image.open "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/original/mapmarker.png"
       first_image.resize "36x36^"
+      first_image.write tmp1.path
       second_image = MiniMagick::Image.open "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/navbar/user_1.png" #@file.path if resized
-      result = first_image.composite(second_image) do |c|
+      result = tmp1.composite(second_image) do |c|
         c.compose "Over" # OverCompositeOp
         c.geometry "+2+2" # copy second_image onto first_image from (2, 2)
       end
