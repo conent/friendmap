7f773bfeac3f7477dee70330ade4fd9045a9903f
Conent <albertocontento@gmail.com>
2013-11-11 12:43:34 +0000

    fix creazione marker mobile side

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,10 @@ class PagesController < ApplicationController
 	    if ((friend.latitude != nil && friend.longitude != nil) && !(friend.latitude == 0 && friend.longitude == 0))
 	      marker.lat friend.latitude
 	      marker.lng friend.longitude
+        lasturl = ""
         if (friend.picture.exists?)
           filename = "user_".concat(friend.id).concat(".png")
+          lasturl = "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/marker/".concat(filename)
           marker.picture({
           "url" => "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/marker/".concat(filename),
           "width" => 30,
@@ -23,7 +25,7 @@ class PagesController < ApplicationController
           })
         end
 
-	      string = "<p>".concat(friend.name).concat(" ").concat(friend.surname).concat("</p> <p>").concat(friend.address).concat("</p>")
+	      string = "<p>".concat(friend.name).concat(" ").concat(friend.surname).concat("</p> <p>").concat(friend.address).concat("</p>").concat(lasturl)
 	      marker.infowindow string
     	end
 	  end
