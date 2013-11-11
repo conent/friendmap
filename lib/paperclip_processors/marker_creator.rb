9c939220d44db06d8e42460f7c827ffa2165af91
Conent <albertocontento@gmail.com>
2013-11-11 11:12:48 +0000

    tentaivo fix creazione marker

--- lib/paperclip_processors/marker_creator.rb
+++ lib/paperclip_processors/marker_creator.rb
@@ -37,25 +37,25 @@ module Paperclip
       begin
         parameters = []
          
-         parameters << ':source'
-         parameters << ':mask'
+         parameters << ':background'
+         parameters << ':source'         
         # parameters << '-alpha'
         # parameters << 'on'
         # parameters << '-compose'
         # parameters << 'CopyOpacity'
-         parameters << 'composite'
-         parameters << '-geometry +5+10'
+         parameters << '-geometry 24x24+5+10'
+         parameters << '-composite'
          parameters << ':dest'
 
         parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
  
         #mask_path = File.expand_path('https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/original/mapmarkersmall.png')
-        success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}[0]", :mask => "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/original/mapmarkersmall.png[0]", :dest => File.expand_path(dst.path))
+        success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}[0]", :background => "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/original/mapmarkersmall.png[0]", :dest => File.expand_path(dst.path))
        
  
  
        rescue PaperclipCommandLineError => e
-         raise PaperclipError, "There was an error during the mask for #{@basename}" if @whiny
+         raise PaperclipError, "There was an error during the marker creation for #{@basename}" if @whiny
        end
  
        dst
