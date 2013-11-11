7f4325e66b437b49cee72f69c69f498c129ab911
Conent <albertocontento@gmail.com>
2013-11-11 12:29:35 +0000

    fix mappa marker web side

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,12 +7,22 @@ class PagesController < ApplicationController
   	@hash = Gmaps4rails.build_markers(friendsOnline) do |friend, marker|
 	    if ((friend.latitude != nil && friend.longitude != nil) && !(friend.latitude == 0 && friend.longitude == 0))
 	      marker.lat friend.latitude
-	      marker.lng friend.longitude  
-        marker.picture({
-          "url" => "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/original/mapmarker.png",
-          "width" => 36,
-          "height" => 36
-        })
+	      marker.lng friend.longitude
+        if (friend.picture.exists?)
+          filename = "user_".concat(friend.id).concat(".png")
+          marker.picture({
+          "url" => "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/marker/".concat(filename),
+          "width" => 30,
+          "height" => 40
+          })
+        else
+          marker.picture({
+          "url" => "https://s3-us-west-2.amazonaws.com/friendmap/app/public/listimages/marker/mapmarkerdefault.png",
+          "width" => 30,
+          "height" => 40
+          })
+        end
+
 	      string = "<p>".concat(friend.name).concat(" ").concat(friend.surname).concat("</p> <p>").concat(friend.address).concat("</p>")
 	      marker.infowindow string
     	end
